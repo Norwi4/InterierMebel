8c91d0c11bafe4319c635def4a2cfab1d7a6251e
Evgeny <abaevevgenchik@gmail.com>
2020-07-16 20:47:42 +0300

    лучшая корзина

--- shop/admin.py
+++ shop/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from .models import Category, Product
 
 
@@ -9,10 +11,20 @@ class CategoryAdmin(admin.ModelAdmin):
 admin.site.register(Category, CategoryAdmin)
 
 
+@admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['name', 'slug', 'price', 'stock', 'available', 'created', 'updated']
+    list_display = ['category', 'name', 'get_image', 'slug', 'price', 'stock', 'available', 'created', 'updated']
     list_filter = ['available', 'created', 'updated']
     list_editable = ['price', 'stock', 'available']
     prepopulated_fields = {'slug': ('name',)}
+    search_fields = ('name',)
+    readonly_fields = ('get_image', )
 
-admin.site.register(Product, ProductAdmin)
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} width="auto" height="50"')
+    get_image.short_description = "Изображение"
+
+#admin.site.register(Product, ProductAdmin)
+
+
+
--- shop/models.py
+++ shop/models.py
@@ -30,6 +30,7 @@ class Product(models.Model):
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    hot = models.BooleanField(default=False)
 
     class Meta:
         ordering = ('name',)
